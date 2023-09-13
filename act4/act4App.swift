841a23a6f952cebe0ae975f46e045c2851fb22ca
Fernanda Guzman <fermaguzmann@gmail.com>
2023-09-13 17:59:30 -0600

    Initial Commit

---
+++ act4/act4App.swift
@@ -0,0 +1,17 @@
+//
+//  act4App.swift
+//  act4
+//
+//  Created by Fernanda M. Guzmán on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct act4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
